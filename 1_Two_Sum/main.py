ce7ff808ae81692c7c97673dd79ccb17f5e8d801
Tamás Háda <tamas.hada@gmail.com>
2024-03-14 14:46:47 +0100

    Add solution for 1. Two Sum

---
+++ 1_Two_Sum/main.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        m = {}
+        for i in range(len(nums)):
+            if nums[i] in m:
+                return [m[nums[i]],i]
+            else:
+                m[target-nums[i]] = i
